a6aafa0717c0fd911a009ad47d19b520817b17f1
ga96wek <marcel.wagenlaender@tum.de>
2019-06-05 14:10:28 +0200

    add initial query_store function

--- crawl_twitter.py
+++ crawl_twitter.py
@@ -1,20 +1,52 @@
 import numpy as np
+import json
+import twitter
+from datetime import date, timedelta
+from pymongo import MongoClient
 
 
-def read_sp500():
-	sp500_path = "files/sp500_constituents.csv"
+def read_sp500(path):
+    sp500 = np.genfromtxt(path, dtype=None, delimiter=",", names=True, encoding="utf8")
+    filter = sp500[:]["conm"]=="S&P 500 Comp-Ltd"
+    sp500 = sp500[filter]
+    filter = sp500[:]["thru"]==""
+    sp500 = sp500[filter]
+    
+    return sp500
 
-	sp500 = np.genfromtxt(sp500_path, dtype=None, delimiter=",", names=True, encoding="utf8")
-	filter = sp500[:]["conm"]=="S&P 500 Comp-Ltd"
-	sp500 = sp500[filter]
-	filter = sp500[:]["thru"]==""
-	sp500 = sp500[filter]
-	
-	return sp500
+
+def query_store(sp500):
+    #mongodb
+    client = MongoClient()
+    db = client.twitterdb
+    #twitter
+    with open("files/access_token.json") as access_token_file:
+        json_object = json.load(access_token_file)
+    api = twitter.Api(consumer_key=json_object["consumer_key"],
+                  consumer_secret=json_object["consumer_secret"],
+                  access_token_key=json_object["access_token_key"],
+                  access_token_secret=json_object["access_token_secret"])
+
+    companies = sp500["co_tic"].tolist()
+
+    for company in companies:
+        collection = db.aapl
+
+        company_abbreviation = "aapl"
+        since = date.today() - timedelta(days=1)
+        until = date.today()
+        query = "q=" + company_abbreviation + "%20since%3A" + str(since) + "%20until%3A" + str(until)
+        results = api.GetSearch(raw_query=query)
+        for tweet in results:
+            tweet_json = json.loads(str(tweet))
+            tweet_id = collection.insert_one(tweet_json).inserted_id
 
 
 def main():
-    sp500 = read_sp500()
+    path = "files/sp500_constituents.csv"
+
+    sp500 = read_sp500(path)
+    query_store(sp500)
 
 
 if __name__ == '__main__':
