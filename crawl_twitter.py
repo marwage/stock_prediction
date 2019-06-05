debbb1a828a5f8d85105a1ae73069a57a41f643c
ga96wek <marcel.wagenlaender@tum.de>
2019-06-05 13:49:34 +0200

    add method read_sp500 to crawl twitter

--- crawl_twitter.py
+++ crawl_twitter.py
@@ -1,17 +1,20 @@
-import csv
+import numpy as np
+
+
+def read_sp500():
+	sp500_path = "files/sp500_constituents.csv"
+
+	sp500 = np.genfromtxt(sp500_path, dtype=None, delimiter=",", names=True, encoding="utf8")
+	filter = sp500[:]["conm"]=="S&P 500 Comp-Ltd"
+	sp500 = sp500[filter]
+	filter = sp500[:]["thru"]==""
+	sp500 = sp500[filter]
+	
+	return sp500
 
 
 def main():
-    # [(name, symbol)]
-    sp500 = []
-
-    with open('sp500.csv', 'r') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=';')
-        for row in csv_reader:
-           assert len(row) == 2
-           sp500.append((row[0].replace('\ufeff', ''), row[1]))
-
-    #next
+    sp500 = read_sp500()
 
 
 if __name__ == '__main__':
--- replace_ufeff.py
+++ replace_ufeff.py
@@ -2,12 +2,13 @@ import csv
 
 
 def main():
-    file_name = 'sp500.csv'
+    file_name = 'files/sp500_constituents.csv'
     file_as_string = ''
 
     with open(file_name, 'r') as file:
         file_as_string = file.read()
-        file_replaced = file_as_string.replace('\ufeff', '')
+
+    file_replaced = file_as_string.replace('\ufeff', '')
 
     with open(file_name, 'w') as file:
         file.write(file_replaced)
