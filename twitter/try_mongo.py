a2675e7e45ad41b91044f778d0f06a2c2eb9df34
ga96wek <marcel.wagenlaender@tum.de>
2019-06-05 12:09:55 +0200

    replace tabs with spaces

--- twitter/try_mongo.py
+++ twitter/try_mongo.py
@@ -4,32 +4,32 @@ from datetime import date, timedelta
 from pymongo import MongoClient
 
 def main():
-	client = MongoClient()
-	db = client.twitterdb
-	collection = db.aapl
+    client = MongoClient()
+    db = client.twitterdb
+    collection = db.aapl
 
-	###
-	with open("access_token.json") as access_token_file:
-		json_object = json.load(access_token_file)
+    ###
+    with open("access_token.json") as access_token_file:
+        json_object = json.load(access_token_file)
 
-	api = twitter.Api(consumer_key=json_object["consumer_key"],
+    api = twitter.Api(consumer_key=json_object["consumer_key"],
                   consumer_secret=json_object["consumer_secret"],
                   access_token_key=json_object["access_token_key"],
                   access_token_secret=json_object["access_token_secret"])
 
-	company_abbreviation = "aapl"
-	since = date.today() - timedelta(days=1)
-	until = date.today()
-	query = "q=" + company_abbreviation + "%20since%3A" + str(since) + "%20until%3A" + str(until)
-	results = api.GetSearch(raw_query=query)
-	###
+    company_abbreviation = "aapl"
+    since = date.today() - timedelta(days=1)
+    until = date.today()
+    query = "q=" + company_abbreviation + "%20since%3A" + str(since) + "%20until%3A" + str(until)
+    results = api.GetSearch(raw_query=query)
+    ###
 
-        with open("query_results.json", "w") as query_results:
-            query_results.write(results)
+    with open("query_results.json", "w") as query_results:
+        query_results.write(results)
 
-	result = collection.insert_many(results)
-	print(result.inserted_ids)
+    result = collection.insert_many(results)
+    print(result.inserted_ids)
 
 
 if __name__ == '__main__':
-	main()
+    main()
