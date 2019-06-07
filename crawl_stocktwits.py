a047c33f9b2cf4bc8b206d49c1d1e460331de120
ga96wek <marcel.wagenlaender@tum.de>
2019-06-07 13:07:14 +0200

    add initial adjustments for stocktwits

--- crawl_stocktwits.py
+++ crawl_stocktwits.py
@@ -1,11 +1,12 @@
 import numpy as np
 import json
-import twitter
 import time
+import requests
 from datetime import date, timedelta, datetime
 from pymongo import MongoClient
 
 files_path = "/home/wagenlaeder/stock-prediction/files"
+log_file = "crawl_stocktwits.log"
 
 def read_sp500(path):
     sp500 = np.genfromtxt(path, dtype=None, delimiter=",", names=True, encoding="utf8")
@@ -21,39 +22,32 @@ def query_store(sp500):
     #mongodb
     client = MongoClient()
     db = client.stocktwitsdb
-    #twitter
-    with open(files_path + "/access_token.json") as access_token_file:
-        json_object = json.load(access_token_file)
-    api = twitter.Api(consumer_key=json_object["consumer_key"],
-                  consumer_secret=json_object["consumer_secret"],
-                  access_token_key=json_object["access_token_key"],
-                  access_token_secret=json_object["access_token_secret"])
 
     companies = sp500["co_tic"].tolist()
 
     for company in companies:
         since = date.today() - timedelta(days=1)
         until = date.today()
-        query = "q=" + "%24"+ company + "%20since%3A" + str(since) + "%20until%3A" + str(until)
+        request_url = "https://api.stocktwits.com/api/2/streams/symbol/" + company + ".json"
         exceeded = True
         while exceeded:
-            try:
-                results = api.GetSearch(raw_query=query)
+            result = requests.get(request_url)
+            print(result.status_code)
+            if result.status_code == 200:
                 exceeded = False
-            except:
+            if result.status_code == 429:
                 # log
-                with open(files_path + "/crawl_twitter.log", "a") as log:
+                with open(files_path + log_file, "a") as log:
                     log.write("sleeping for 16 min\n")
-                time.sleep(960)
+                time.sleep(3660)
 
         # log
-        with open(files_path + "/crawl_twitter.log", "a") as log:
+        with open(files_path + log_file, "a") as log:
             log.write(str(len(results)) + " results for " + company + "\n")
 
         collection = db[company]
-        for tweet in results:
-            tweet_json = json.loads(str(tweet))
-            write_result = collection.update(tweet_json, tweet_json, upsert=True)
+        for post in result.json()["messages"]:
+            write_result = collection.update(post, post, upsert=True)
 
 
 def main():
@@ -63,7 +57,7 @@ def main():
     query_store(sp500)
 
     # log
-    with open(files_path + "/crawl_twitter.log", "a") as log:
+    with open(files_path + log_file, "a") as log:
         log.write(str(datetime.now()) + " finished\n")
 
 
