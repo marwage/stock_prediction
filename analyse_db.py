959769d847a05162b6fc2e45467142b7c18d2d66
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2019-09-02 12:28:18 +0000

    add stats of stocktwits

--- analyse_db.py
+++ analyse_db.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from datetime import datetime
+import json
 
 
 def main():
@@ -9,17 +11,19 @@ def main():
 
     stats = dict()
     for share_code in db.list_collection_names():
-        collection = db.share_code
+        collection = db[share_code]
         stats[share_code] = dict()
-        for post in collection.find():
+        for post in collection.find({}):
             date_string = post['created_at']
-            date = datetime.strptime(date_string[0:10], '%Y-%m-%d')
-            if date in stats[share_code]:
-                stats[share_code][date] = stats[share_code][date] + 1
+            post_date = datetime.strptime(date_string[0:10], '%Y-%m-%d')
+            date_string = post_date.strftime('%Y-%m-%d')
+            if date_string in stats[share_code]:
+                stats[share_code][date_string] = stats[share_code][date_string] + 1
             else:
-                stats[share_code][date] = 1
+                stats[share_code][date_string] = 1
 
-    print(stats)
+    with open('stocktwits_stats.json', 'w') as json_file:
+        json.dump(stats, json_file)
 
 
 if __name__ == '__main__':
