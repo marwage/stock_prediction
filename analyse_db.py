246a7fefe4e89fe4a688f09a230d80e30f2c16c8
ga96wek <marcel.wagenlaender@tum.de>
2019-09-02 13:55:06 +0200

    change to spaces

--- analyse_db.py
+++ analyse_db.py
@@ -2,23 +2,23 @@ from pymongo import MongoClient
 
 
 def main():
-	client = MongoClient()
+    client = MongoClient()
     
     # stocktwits
     db = client.stocktwitsdb
 
     stats = dict()
-	for share_code in db.list_collection_names():
-		collection = db.share_code
-		stats[share_code] = dict()
-		for post in collection.find():
-			date_string = post['created_at']
-			date = datetime.strptime(date_string[0:10], '%Y-%m-%d')
-			if date in stats[share_code]:
-				stats[share_code][date] = stats[share_code][date] + 1
-			else:
-				stats[share_code][date] = 1
+    for share_code in db.list_collection_names():
+        collection = db.share_code
+        stats[share_code] = dict()
+        for post in collection.find():
+            date_string = post['created_at']
+            date = datetime.strptime(date_string[0:10], '%Y-%m-%d')
+            if date in stats[share_code]:
+                stats[share_code][date] = stats[share_code][date] + 1
+            else:
+                stats[share_code][date] = 1
 
 
 if __name__ == '__main__':
-	main()
+    main()
