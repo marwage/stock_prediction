c8b4b593f0dc0f5978ccd4cd469fea8030d033fb
ga96wek <marcel.wagenlaender@tum.de>
2019-09-05 20:44:14 +0200

    add while loop if error occures

--- query_alpha_vantage.py
+++ query_alpha_vantage.py
@@ -2,6 +2,7 @@ import requests
 import json
 from pymongo import MongoClient
 from datetime import datetime
+import time
 
 
 def read_sp500(path):
@@ -32,25 +33,35 @@ def query_stock_price(apikey, sp500):
     db = client.stockpricedb
 
     for company in sp500:
-        request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + company + "&interval=5min&apikey=" + apikey
-        result = requests.get(request_url)
-        if result.status_code == 200:
-            collection = db[company]
-            for key, value in result.json()["Time Series (Daily)"].items():
-                date = datetime.strptime(key, "%Y-%m-%d")
-                if date > datetime(2019, 6, 1):
-                    entry = dict()
-                    entry[key] = dict()
-                    entry[key]["open"] = float(value["1. open"])
-                    entry[key]["high"] = float(value["2. high"])
-                    entry[key]["low"] = float(value["3. low"])
-                    entry[key]["close"] = float(value["4. close"])
-                    entry[key]["volume"] = float(value["5. volume"])
-                    
-                    write_result = collection.update(entry, entry, upsert=True)
+        sucessful = False
+        while not sucessful:
+            request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + company + "&interval=5min&apikey=" + apikey
+            result = requests.get(request_url)
+            if result.status_code == 200:
+                try:
+                    items = result.json()["Time Series (Daily)"].items()
+                    sucessful = True
+                except Exception as e:
+                    write_to_log(str(e))
+                    time.sleep(60)
+                    break
+
+                collection = db[company]
+                for key, value in result.json()["Time Series (Daily)"].items():
+                    date = datetime.strptime(key, "%Y-%m-%d")
+                    if date > datetime(2019, 6, 1):
+                        entry = dict()
+                        entry[key] = dict()
+                        entry[key]["open"] = float(value["1. open"])
+                        entry[key]["high"] = float(value["2. high"])
+                        entry[key]["low"] = float(value["3. low"])
+                        entry[key]["close"] = float(value["4. close"])
+                        entry[key]["volume"] = float(value["5. volume"])
+                        
+                        write_result = collection.update(entry, entry, upsert=True)
 
         else:
-            write_to_log("error " + result.status_code)
+            write_to_log(str(result))
 
 
 def main():
