2a4ed7efb696ce60e5a0ef5ef12bd72c13fac076
ga96wek <marcel.wagenlaender@tum.de>
2019-09-11 11:28:05 +0200

    plot for every company

--- plot_stats.py
+++ plot_stats.py
@@ -1,34 +1,43 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from util import read_sp500
 
 
 def main():
-    with open("files/stocktwits_stats.json", "r") as json_file:
+    files_path = "files/"
+    stats_path = files_path + "stocktwits_stats.json"
+    sp500_path = files_path + "sp500.json"
+    save_path = files_path + "graphs/stocktwits/"
+
+    sp500 = read_sp500(sp500_path)
+
+    with open(stats_path, "r") as json_file:
         stats = json.load(json_file)
 
-    apple = stats["AAPL"]
-    n = 0
-    dates = []
-    counts = []
-    for key, value in apple.items():
-        dates.append(key)
-        counts.append(value)
-        n = n + 1
+    for company in sp500:
+        company_stats = stats[company]
+        n = 0
+        dates = []
+        counts = []
+        for key, value in company_stats.items():
+            dates.append(key)
+            counts.append(value)
+            n = n + 1
 
-    ind = np.arange(n) * 1
-    height = 0.5
+        ind = np.arange(n) * 1
+        height = 0.5
 
-    fig, ax = plt.subplots()
-    ax.tick_params(axis="y", labelsize=4)
+        fig, ax = plt.subplots()
+        ax.tick_params(axis="y", labelsize=4)
 
-    p1 = plt.barh(ind, counts, height)
+        p1 = plt.barh(ind, counts, height)
 
-    plt.xlabel("Counts")
-    plt.title("AAPL counts per day")
-    plt.yticks(ind, dates)
+        plt.xlabel("Counts")
+        plt.title(company + " messages per day")
+        plt.yticks(ind, dates)
 
-    plt.savefig("files/stocktwits_AAPL.svg")
+        plt.savefig(save_path + company + ".svg")
 
 
 if __name__ == '__main__':
