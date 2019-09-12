e559808eb012248fea020e1c18759f090b3e22a1
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2019-09-12 09:19:37 +0000

    print write results in try update

--- try_update.py
+++ try_update.py
@@ -6,22 +6,21 @@ from pymongo import MongoClient
 def try_update():
     client = MongoClient()
     db = client.stocktwitsdb
+    collection = db["GOOGL"]
 
-    request_url = "https://api.stocktwits.com/api/2/streams/symbol/AAPL.json"     
+    request_url = "https://api.stocktwits.com/api/2/streams/symbol/GOOGL.json"     
         
     try:
         result = requests.get(request_url)
-
         if result.status_code == 200: # request was successful
             # add all messages to the database
-            collection = db["AAPL"]
             for post in result.json()["messages"]:
-                query = dict()
-                query["body"] = post["body"]
-                query["created_at"] = post["created_at"]
-                for ob in collection.find(query):
-                    print(ob)
+                query = {
+                    "body": post["body"],
+                    "created_at": post["created_at"]
+                    }
                 write_result = collection.update(query , post, upsert=True)
+                print(write_result)
     except Exception as e:
         print(e)
 
