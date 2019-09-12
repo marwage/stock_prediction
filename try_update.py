61bfa33dc3a045576043ffe19c1d1f00aadf2f12
ga96wek <marcel.wagenlaender@tum.de>
2019-09-12 11:02:46 +0200

    add script to try update method of mongo

---
+++ try_update.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+
+def try_update():
+    request_url = "https://api.stocktwits.com/api/2/streams/symbol/AAPL.json"     
+        
+    try:
+        result = requests.get(request_url)
+
+        if result.status_code == 200: # request was successful
+            # add all messages to the database
+            collection = db["AAPL"]
+            for post in result.json()["messages"]:
+                write_result = collection.update({"body": post["body"], "created_at": post["created_at"]} , post, upsert=True)
+                print(str(post)[0:20])
+    except Exception as e:
+        print(e)
+
+
+def main():
+    try_update()
+
+
+if __name__ == '__main__':
+    main()
