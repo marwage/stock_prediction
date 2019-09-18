5e4acb9ff7a279a322f94a4cda4092ceedef29f7
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2019-09-18 15:03:41 +0200

    update remove_ufeff

--- util/remove_ufeff.py
+++ util/remove_ufeff.py
@@ -2,15 +2,14 @@ import csv
 
 
 def main():
-    file_name = 'files/sp500_constituents.csv'
-    file_as_string = ''
+    file_name = "files/sp500_constituents.csv"
 
-    with open(file_name, 'r') as file:
+    with open(file_name, "r") as file:
         file_as_string = file.read()
 
     file_replaced = file_as_string.replace('\ufeff', '')
 
-    with open(file_name, 'w') as file:
+    with open(file_name, "w") as file:
         file.write(file_replaced)
 
 
