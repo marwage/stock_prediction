13da36c1b2731bea55071e04f830da9222eb7585
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2019-09-19 11:50:47 +0200

    add missing lines

--- sentiment_analysis/posts_to_array.py
+++ sentiment_analysis/posts_to_array.py
@@ -18,6 +18,20 @@ def words_to_index(word_index, words):
 
 
 def main():
+    # download imdb dataset
+    imdb = keras.datasets.imdb
+    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=25000)
+
+    # dictionary mapping words to an integer index
+    word_index = imdb.get_word_index()
+
+    # The first indices are reserved
+    word_index = {k:(v+3) for k,v in word_index.items()}
+    word_index["<PAD>"] = 0
+    word_index["<START>"] = 1
+    word_index["<UNK>"] = 2  # unknown
+    word_index["<UNUSED>"] = 3
+    
     #load stocktwits posts
     client = MongoClient()
     db = client["stocktwitsdb"]
