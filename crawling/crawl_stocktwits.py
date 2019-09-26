7da2ae2920acbf80d48d9ecdccddcf0e233564ed
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2019-09-26 13:22:13 +0000

    change divide in chunks but I'm not sure if that was correct but it's working again

--- crawling/crawl_stocktwits.py
+++ crawling/crawl_stocktwits.py
@@ -17,10 +17,7 @@ def get_working_proxies(path):
 
 def divide_in_chunks(lis, num):
     length = len(lis)
-    if length % num != 0:
-        chunk_size = (length // num) + 1
-    else:
-        chunk_size = (length // num)
+    chunk_size = (length // num)
     for i in range(0, length, chunk_size): 
         yield lis[i : i+chunk_size] 
 
