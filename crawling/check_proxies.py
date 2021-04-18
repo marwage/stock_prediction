775488037d769d9803f80fe56646acb0d1cde328
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2021-04-18 05:59:19 +0000

    correct paths; mute logging of imported modules

--- crawling/check_proxies.py
+++ crawling/check_proxies.py
@@ -1,6 +1,9 @@
 import json
 import logging
+import os
 import requests
+import sys
+from pathlib import Path
 
 
 def get_proxies(path):
@@ -38,16 +41,24 @@ def check_proxies(proxies, output_path):
 
 
 def main():
-    crawling_path = "stock-prediction/crawling/"
-    proxy_path = crawling_path + "data/proxy_list.txt"
-    output_path = crawling_path + "data/working_proxies.json"
-    log_path = crawling_path + "log/check_proxies.log"
+    if sys.platform == "linux":
+        path = os.path.join(Path.home(), "stock-prediction")
+    else:
+        print("Wrong OS")
+        return
+    crawling_path = os.path.join(path, "crawling")
+    log_path = os.path.join(crawling_path, "log/check_proxies.log")
+    proxy_path = os.path.join(crawling_path, "data/proxy_list.txt")
+    output_path = os.path.join(crawling_path, "data/working_proxies.json")
 
     logging.basicConfig(
         filename=log_path,
         level=logging.DEBUG,
         format="%(asctime)s:%(levelname)s:%(message)s"
         )
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
 
     proxies = get_proxies(proxy_path)
     check_proxies(proxies, output_path)
