0ceb5c942f788087d139803ac60ca06643c5f9a6
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2021-04-19 06:43:01 +0000

    finish count tweets script

--- stats/count_tweets.py
+++ stats/count_tweets.py
@@ -1,66 +1,75 @@
 import datetime
+import json
 import logging
 import os
 import pandas as pd
 import sys
 from pathlib import Path
 from pymongo import MongoClient
-from read_sp500 import read_sp500
 
 
-def check(sp500: list, output_path: str):
+def read_sp500(path):
+    with open(path, "r") as json_file:
+        sp500_json = json.load(json_file)
+
+    return sp500_json["sp500"]
+
+
+def check(sp500: list, database_name: str, output_path: str):
     client = MongoClient()
-    stockprice_db = client["stockpricedb"]
+    database = client[database_name]
 
     for company in sp500:
-        collection = stockprice_db[company]
+        collection = database[company]
 
         days = []
-        has_entry = []
+        tweet_count = []
 
         start_day = datetime.datetime(2019, 4, 1)
         end_day = datetime.datetime.now()
         current_day = start_day
 
         while current_day < end_day:
-            found_days = collection.count_documents({"date": current_day})
-            if found_days == 0:
-                days.append(current_day)
-                has_entry.append(False)
-            elif found_days == 1:
-                days.append(current_day)
-                has_entry.append(True)
-            else:  # should not be the case
-                logging.error("Database has duplicates")
+            logging.debug("Check %s on %s", company, current_day)
+            next_day = current_day + datetime.timedelta(days=1)
+            num_tweets = collection.count_documents(
+                    {"date": {"$gte": current_day,
+                              "$lt": next_day}})
+            days.append(current_day)
+            tweet_count.append(num_tweets)
 
             current_day = current_day + datetime.timedelta(days=1)
 
-        data_frame = pd.DataFrame({"day": days, "has_entry": has_entry})
-        data_frame.to_csv(os.path.join(output_path, "{}.csv".format(company)))
+        data_frame = pd.DataFrame({"day": days, "tweet_count": tweet_count})
+        file_name = "tweet_count_{}_{}.csv".format(database_name, company)
+        data_frame.to_csv(os.path.join(output_path, file_name))
 
 
 def main():
     if sys.platform == "linux":
-        crawling_path = os.path.join(Path.home(), "stock-prediction/crawling")
+        path = os.path.join(Path.home(), "stock-prediction")
     else:
-        directory = "Studies/Master/10SS19/StockPrediction/" \
-                  + "stock-prediction/crawling"
-        crawling_path = os.path.join(Path.home(), directory)
+        path = os.path.join(Path.home(),
+                            "Studies/Master/10SS19/StockPrediction")
+        path = os.path.join(path, "stock-prediction")
+    stats_path = os.path.join(path, "stats")
+    crawling_path = os.path.join(path, "crawling")
     sp500_path = os.path.join(crawling_path, "data/sp500.json")
-    log_path = os.path.join(crawling_path, "log/stock_price.log")
-    output_path = os.path.join(crawling_path, "output")
+    log_path = os.path.join(stats_path, "log/count_tweets.log")
+    output_path = os.path.join(stats_path, "output")
 
     logging.basicConfig(
         filename=log_path,
         level=logging.DEBUG,
-        format="%(asctime)s:%(levelname)s:%(message)s"
-        )
+        format="%(asctime)s:%(levelname)s:%(message)s")
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
     sp500 = read_sp500(sp500_path)
-    check(sp500, output_path)
+    databases = ["twitterdb", "stocktwitsdb"]
+    for database in databases:
+        check(sp500, database, output_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
