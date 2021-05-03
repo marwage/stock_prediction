1ed1ac73f0eaef24c078160af2fec5c1c0f5a9de
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2021-05-03 16:04:25 +0000

    ckpt cleanup preprocessing

--- preprocessing/add_date.py
+++ preprocessing/add_date.py
@@ -1,11 +1,8 @@
+import datetime
 import json
-import logging
-from datetime import datetime
 import os
-from pathlib import Path
-from pymongo import MongoClient
 import re
-import sys
+from pymongo import MongoClient
 
 
 def read_sp500(path):
@@ -15,44 +12,42 @@ def read_sp500(path):
     return sp500_json["sp500"]
 
 
-def add_dates(do_twitter_db: bool, do_stocktwits_db: bool):
+def add_dates(data_base_name: str, sp500: list):
     client = MongoClient()
-    learning_db = client["learning"]
     twitter_db = client["twitterdb"]
     stocktwits_db = client["stocktwitsdb"]
 
-    if sys.platform == "linux":
-        path = os.path.join(Path.home(), "stock-prediction")
-    else:
-        path = os.path.join(Path.home(), "Studies/Master/10SS19/StockPrediction/stock-prediction")
-    sp500_path = os.path.join(path, "crawling/data/sp500.json")
-    sp500 = read_sp500(sp500_path)
 
-    if do_twitter_db:
+    if data_base_name == "twitterdb":
         for company in sp500:
             collection = twitter_db[company]
             tweets = collection.find()
             for post in tweets:
                 if "date" not in post:
-                    d = datetime.strptime(post["created_at"], "%a %b %d %H:%M:%S %z %Y")
-                    collection.update_one({"_id": post["_id"]}, {"$set": {"date": d}})
-
-    if do_stocktwits_db:
+                    parse_pattern = "%a %b %d %H:%M:%S %z %Y"
+                    date = datetime.datetime.strptime(post["created_at"],
+                                                      parse_pattern)
+                    collection.update_one({"_id": post["_id"]},
+                                          {"$set": {"date": date}})
+    elif data_base_name == "stocktwitsdb":
         for company in sp500:
             collection = stocktwits_db[company]
             tweets = collection.find()
             for post in tweets:
                 if "date" not in post:
-                    d = datetime.fromisoformat(re.sub("Z", "", post["created_at"]))
-                    collection.update_one({"_id": post["_id"]}, {"$set": {"date": d}})
+                    date_str = re.sub("Z", "", post["created_at"])
+                    date = datetime.datetime.fromisoformat(date_str)
+                    collection.update_one({"_id": post["_id"]},
+                                          {"$set": {"date": date}})
 
 
 def main():
-    do_twitter_db = True
-    do_stocktwits_db = False
-    add_dates(do_twitter_db, do_stocktwits_db)
+    sp500_path = os.path.join("../crawling", "data/sp500.json")
+
+    sp500 = read_sp500(sp500_path)
+    add_dates("twitterdb", sp500)
+    add_dates("stocktwitsdb", sp500)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
